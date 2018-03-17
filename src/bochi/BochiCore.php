761a81b0dfafcfd8828def42871dcaeabf90ae4c
Rice_Field <famima65536@gmail.com>
2018-03-17 11:37:30 +0900

    起動時に実行する関数の追加

--- src/bochi/BochiCore.php
+++ src/bochi/BochiCore.php
@@ -9,6 +9,7 @@
 namespace bochi;
 
 
+use Composer\Config;
 use pocketmine\plugin\PluginBase;
 
 class BochiCore extends PluginBase
@@ -22,6 +23,20 @@ class BochiCore extends PluginBase
             "This project is developed by %s, and you.",
             implode(" ", $description->getAuthors())
         ));
+
+        if(!file_exists($this->getDataFolder()."setting.yml")) { // 以前に起動してなかったら
+            $this->setup();
+        }
+
+        $this->loadSetting();
+    }
+
+    public function setup() {
+        // TODO: file copy.
+    }
+
+    public function loadSetting() {
+        // TODO: file load
     }
 
 }
