b7068b2e5fdf435b456c71c888689539a19214b3
Rice_Field <famima65536@gmail.com>
2018-03-17 16:29:54 +0900

    add ItemCountTask
    アイテム数を数えるタスクをつくる

---
+++ src/bochi/task/ItemCountTask.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tokai
+ * Date: 2018/03/17
+ * Time: 16:28
+ */
+
+namespace bochi\task;
+
+
+use pocketmine\scheduler\AsyncTask;
+
+class ItemCountTask extends AsyncTask
+{
+
+    /**
+     * Actions to execute when run
+     *
+     * @return void
+     */
+    public function onRun()
+    {
+        // TODO: Implement onRun() method.
+    }
+}
