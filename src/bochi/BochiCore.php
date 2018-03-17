1a25d7e2e0ce38e88e7ac4311e00610608dc980f
Rice_Field <famima65536@gmail.com>
2018-03-17 10:21:38 +0900

    開発者名表示機能の追加
    開発者の名前を起動時に表示にするようにしました

--- src/bochi/BochiCore.php
+++ src/bochi/BochiCore.php
@@ -16,7 +16,12 @@ class BochiCore extends PluginBase
 
     public function onEnable()
     {
+        $description = $this->getDescription();
         $this->getLogger()->info("Hello, Bochi core Alpha.");
+        $this->getLogger()->info(sprintf(
+            "This project is developed by %s, and you.",
+            implode(" ", $description->getAuthors())
+        ));
     }
 
 }
