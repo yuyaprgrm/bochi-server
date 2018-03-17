0543bc4739d5acdf50892becab4056f0f0206bd2
Rice_Field <famima65536@gmail.com>
2018-03-17 17:42:13 +0900

    アイテムカウンタ実装

--- src/bochi/task/ItemCountTask.php
+++ src/bochi/task/ItemCountTask.php
@@ -9,10 +9,23 @@
 namespace bochi\task;
 
 
+use pocketmine\item\Item;
 use pocketmine\scheduler\AsyncTask;
+use pocketmine\Server;
 
 class ItemCountTask extends AsyncTask
 {
+
+    private $name;
+    private $items; /** @var Item[]  */
+    private $targets; /** @var Item[] */
+
+    public function  __construct(string $name, array $items, array $targets)
+    {
+        $this->name = $name;
+        $this->items = $items;
+        $this->targets = $targets;
+    }
 
     /**
      * Actions to execute when run
@@ -21,6 +34,24 @@ class ItemCountTask extends AsyncTask
      */
     public function onRun()
     {
-        // TODO: Implement onRun() method.
+        $results = [];
+        foreach ($this->targets as $target) {
+            // Result set.
+            $item_name = $target->getName();
+            $results[$item_name] = 0;
+
+            foreach($this->items as $item) {
+                if($target->equals($item, true, false)) {
+                    $results[$item_name]++;
+                }
+            }
+        }
+
+        $this->setResult($results);
+    }
+
+    public function onCompletion(Server $server)
+    {
+        
     }
 }
