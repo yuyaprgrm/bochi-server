9627ed165b96728259fa24857e38b15f4d1028df
Rice_Field <famima65536@gmail.com>
2018-03-18 18:50:34 +0900

    Display系の追加
    画面描画用クラスです。

---
+++ src/bochi/task/DisplayPopupTask.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tokai
+ * Date: 2018/03/18
+ * Time: 18:49
+ */
+
+namespace bochi\task;
+
+
+use pocketmine\plugin\Plugin;
+use pocketmine\scheduler\PluginTask;
+
+class DisplayPopupTask extends PluginTask
+{
+
+    public function __construct(Plugin $owner)
+    {
+        parent::__construct($owner);
+    }
+
+    /**
+     * Actions to execute when run
+     *
+     * @param int $currentTick
+     *
+     * @return void
+     */
+    public function onRun(int $currentTick)
+    {
+        // TODO: Implement onRun() method.
+    }
+}
---
+++ src/bochi/utils/Display.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tokai
+ * Date: 2018/03/18
+ * Time: 18:48
+ */
+
+namespace bochi\utils;
+
+
+class Display
+{
+    
+}
