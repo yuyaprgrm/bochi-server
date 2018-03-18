43ce997d3f42644dc2624b0f9a9a6818eaaa1d25
Rice_Field <famima65536@gmail.com>
2018-03-18 19:54:58 +0900

    ディスプレイ実装

--- src/bochi/utils/Display.php
+++ src/bochi/utils/Display.php
@@ -9,7 +9,44 @@
 namespace bochi\utils;
 
 
+use pocketmine\Player;
+
 class Display
 {
-    
+
+    private static $displays = [];
+
+    public static function get(Player $player) : Display {
+        $display = Display::$displays[$player->getName()] ?? null;
+
+        if($display == null) {
+            Display::$displays[$player->getName()] = new Display($player);
+            $display = Display::$displays[$player->getName()];
+        }
+
+        return $display;
+    }
+
+    private $name;
+    public $format;
+    public $args;
+
+    public function __construct(Player $player)
+    {
+        $this->name = $player->getName();
+        $this->format = "";
+        $this->args = [];
+    }
+
+    public function getText() {
+        return sprintf($this->format, ...$this->args);
+    }
+
+    public function __get($name)
+    {
+        switch ($name) {
+            case "text":
+                return $this->getText();
+        }
+    }
 }
